91ca5020fbf15760c654621091cb2a2a5490c376
teamdhaksha <63536148+teamdhaksha@users.noreply.github.com>
2021-05-06 17:43:07 +0530

    Add files via upload

---
+++ data/Khair3_5.js
@@ -0,0 +1,10 @@
+var json_Khair3_5 = {
+"type": "FeatureCollection",
+"name": "Khair3_5",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Tree_Type": "Khair", "Height_F": 0.89999997615799998 }, "geometry": { "type": "Point", "coordinates": [ 83.411798466933917, 20.563129334761857 ] } },
+{ "type": "Feature", "properties": { "Tree_Type": "Khair", "Height_F": 4.5 }, "geometry": { "type": "Point", "coordinates": [ 83.412021665082776, 20.563010575307196 ] } },
+{ "type": "Feature", "properties": { "Tree_Type": "Khair", "Height_F": 10.600000381499999 }, "geometry": { "type": "Point", "coordinates": [ 83.412179772883349, 20.563396347424518 ] } }
+]
+}
