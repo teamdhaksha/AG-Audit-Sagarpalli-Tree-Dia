6e42d50091593a78befae19d671b2912df4c34fe
teamdhaksha <63536148+teamdhaksha@users.noreply.github.com>
2021-05-06 17:21:14 +0530

    Add files via upload

---
+++ data/Bamboo_4.js
@@ -0,0 +1,26 @@
+var json_Bamboo_4 = {
+"type": "FeatureCollection",
+"name": "Bamboo_4",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Tree_Type": "Bamboo", "Height_F": 1.3108999729199999 }, "geometry": { "type": "Point", "coordinates": [ 83.412214048309366, 20.563285350328933 ] } },
+{ "type": "Feature", "properties": { "Tree_Type": "Bamboo", "Height_F": 0.89999997615799998 }, "geometry": { "type": "Point", "coordinates": [ 83.412065157621697, 20.563253291068644 ] } },
+{ "type": "Feature", "properties": { "Tree_Type": "Bamboo", "Height_F": 0.10167499631599999 }, "geometry": { "type": "Point", "coordinates": [ 83.412038288872225, 20.563253307976481 ] } },
+{ "type": "Feature", "properties": { "Tree_Type": "Bamboo", "Height_F": 0.89999997615799998 }, "geometry": { "type": "Point", "coordinates": [ 83.411873415067134, 20.563150808237147 ] } },
+{ "type": "Feature", "properties": { "Tree_Type": "Bamboo", "Height_F": 3.5408000945999998 }, "geometry": { "type": "Point", "coordinates": [ 83.411766938575482, 20.563040489092291 ] } },
+{ "type": "Feature", "properties": { "Tree_Type": "Bamboo", "Height_F": 5.30289983749 }, "geometry": { "type": "Point", "coordinates": [ 83.411790593171574, 20.563038801305538 ] } },
+{ "type": "Feature", "properties": { "Tree_Type": "Bamboo", "Height_F": 0.66990000009499995 }, "geometry": { "type": "Point", "coordinates": [ 83.411640491499597, 20.563212313904589 ] } },
+{ "type": "Feature", "properties": { "Tree_Type": "Bamboo", "Height_F": 0.89999997615799998 }, "geometry": { "type": "Point", "coordinates": [ 83.411663707733695, 20.563211704467037 ] } },
+{ "type": "Feature", "properties": { "Tree_Type": "Bamboo", "Height_F": 0.89999997615799998 }, "geometry": { "type": "Point", "coordinates": [ 83.411378028981886, 20.562811436153524 ] } },
+{ "type": "Feature", "properties": { "Tree_Type": "Bamboo", "Height_F": 0.89999997615799998 }, "geometry": { "type": "Point", "coordinates": [ 83.411615509178318, 20.562878183173673 ] } },
+{ "type": "Feature", "properties": { "Tree_Type": "Bamboo", "Height_F": 0.89999997615799998 }, "geometry": { "type": "Point", "coordinates": [ 83.411664339010741, 20.563232569328743 ] } },
+{ "type": "Feature", "properties": { "Tree_Type": "Bamboo", "Height_F": 0.124654002488 }, "geometry": { "type": "Point", "coordinates": [ 83.411736248761841, 20.563244220327284 ] } },
+{ "type": "Feature", "properties": { "Tree_Type": "Bamboo", "Height_F": 0.89999997615799998 }, "geometry": { "type": "Point", "coordinates": [ 83.411738648327102, 20.56322302734748 ] } },
+{ "type": "Feature", "properties": { "Tree_Type": "Bamboo", "Height_F": 0.89999997615799998 }, "geometry": { "type": "Point", "coordinates": [ 83.411784948786632, 20.563242398046444 ] } },
+{ "type": "Feature", "properties": { "Tree_Type": "Bamboo", "Height_F": 0.88620001077699995 }, "geometry": { "type": "Point", "coordinates": [ 83.411787417099077, 20.563264238109674 ] } },
+{ "type": "Feature", "properties": { "Tree_Type": "Bamboo", "Height_F": 7.0 }, "geometry": { "type": "Point", "coordinates": [ 83.410494907929817, 20.56285661941746 ] } },
+{ "type": "Feature", "properties": { "Tree_Type": "Bamboo", "Height_F": 6.0100002288800001 }, "geometry": { "type": "Point", "coordinates": [ 83.409955722544566, 20.562413892579436 ] } },
+{ "type": "Feature", "properties": { "Tree_Type": "Bamboo", "Height_F": 0.66601997613899999 }, "geometry": { "type": "Point", "coordinates": [ 83.411851493592735, 20.563248577045449 ] } },
+{ "type": "Feature", "properties": { "Tree_Type": "Bamboo", "Height_F": 3.0 }, "geometry": { "type": "Point", "coordinates": [ 83.409732564326447, 20.562466760728682 ] } }
+]
+}
