413b39c8ff24d2f5d5f1b2e2442ee4ba700aa673
teamdhaksha <63536148+teamdhaksha@users.noreply.github.com>
2021-05-18 19:27:15 +0530

    Add files via upload

--- data/Khair3_5.js
+++ data/Khair3_5.js
@@ -3,8 +3,8 @@ var json_Khair3_5 = {
 "name": "Khair3_5",
 "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
 "features": [
-{ "type": "Feature", "properties": { "Tree_Type": "Khair", "Height_F": 0.89999997615799998 }, "geometry": { "type": "Point", "coordinates": [ 83.411798466933917, 20.563129334761857 ] } },
-{ "type": "Feature", "properties": { "Tree_Type": "Khair", "Height_F": 4.5 }, "geometry": { "type": "Point", "coordinates": [ 83.412021665082776, 20.563010575307196 ] } },
-{ "type": "Feature", "properties": { "Tree_Type": "Khair", "Height_F": 10.600000381499999 }, "geometry": { "type": "Point", "coordinates": [ 83.412179772883349, 20.563396347424518 ] } }
+{ "type": "Feature", "properties": { "Tree_Type": "Khair", "Height_F": 4.0, "Tree_Growt": 0.66666666666700003 }, "geometry": { "type": "Point", "coordinates": [ 83.411798466933917, 20.563129334761854 ] } },
+{ "type": "Feature", "properties": { "Tree_Type": "Khair", "Height_F": 12.0, "Tree_Growt": 2.0 }, "geometry": { "type": "Point", "coordinates": [ 83.412021665082776, 20.563010575307196 ] } },
+{ "type": "Feature", "properties": { "Tree_Type": "Khair", "Height_F": 16.0, "Tree_Growt": 2.6666666666699999 }, "geometry": { "type": "Point", "coordinates": [ 83.412179772883349, 20.563396347424515 ] } }
 ]
 }
